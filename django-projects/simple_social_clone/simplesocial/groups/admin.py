463cdf494fb975040e32dd8b9d511b0660547092
mtzahn <mtzahn@motorola.com>
2017-08-14 18:21:23 -0300

    More work on groups

--- django-projects/simple_social_clone/simplesocial/groups/admin.py
+++ django-projects/simple_social_clone/simplesocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class GroupMemberInLine(admin.TabularInLine):
+    model = models.GroupMember
+    
+
+admin.site.register(models.Group)
